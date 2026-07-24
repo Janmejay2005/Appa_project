92b5d536afdcd7767c5351dd6a3a961c5c25d9e3
Janmejay Kumar Singh <86195670+Janmejay2005@users.noreply.github.com>
2026-07-24 09:06:43 +0530

    Delete art.py
    This was contributing nothing to the file

--- art.py
+++
@@ -1,4 +0,0 @@
-# First few lines of code and I am going to use tkinter for the frontend and then for the backend I am going to use python 
-# Maybe I am going to use streamlit so that I can also learn it's work and later on I am going to use flask and fastapi later on
-
-
